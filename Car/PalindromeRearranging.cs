a657ca8c0e9a886789b94c5e97c5c41bd7ea6884
Durga Prasad <durga.naganaboina@aveva.com>
2018-03-07 10:13:50 +0530

    adding project to github

---
+++ Car/PalindromeRearranging.cs
@@ -0,0 +1,51 @@
+﻿namespace Car
+{
+    public class PalindromeRearranging
+    {
+        public bool IsPalindromeRearrangin(string value)
+        {
+            if (value.Length % 2 == 0)
+            {
+                foreach (var i in value)
+                {
+                    int count = 0;
+                    foreach (var j in value)
+                    {
+                        if (i == j)
+                        {
+                            count++;
+                        }
+                    }
+
+                    if (count % 2 != 0)
+                        return false;
+                }
+
+                return true;
+            }
+            else
+            {
+                int tmpcount = 0;
+                foreach (var i in value)
+                {
+                    int count = 0;
+                    foreach (var j in value)
+                    {
+                        if (i == j)
+                        {
+                            count++;
+                        }
+                    }
+
+                    if (count % 2 != 0)
+                        tmpcount++;
+                }
+
+                if (tmpcount == 1)
+                    return true;
+                else
+                    return false;
+            }
+        }
+    }
+}
---
+++ Car/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PalindromeRearranging pr = new PalindromeRearranging();
+            Console.WriteLine(pr.IsPalindromeRearrangin("abbcdac"));
+            Console.WriteLine(pr.IsPalindromeRearrangin("abccba"));
+        }
+    }
+}
